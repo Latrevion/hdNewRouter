60247c54611ac3db18fcccd362c9c16e6d24a90f
Latrevion <xiaopikaqiu@outlook.com>
2023-05-13 20:05:49 +0800

    dev router-views

---
+++ base/src/components/RouterView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import {computed} from "vue"
+
+let view = computed(()=>{
+  console.log(123456)
+})
+</script>
+
+<template>
+  <component :is="view"></component>
+</template>
+
+<style scoped>
+
+</style>
