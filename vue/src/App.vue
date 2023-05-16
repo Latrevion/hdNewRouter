5784a0a22c2d1ada67f8aa9a74c81c4548db5e83
Latrevion <xiaopikaqiu@outlook.com>
2023-05-16 20:53:50 +0800

    Route naming and layout analysis

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <router-link to="/">home</router-link> |
-  <router-link to="/article">article</router-link>
+<!--  <router-link to="">home</router-link> |-->
+<!--  <router-link to="/article">article</router-link>-->
+  <router-link :to="{name:'home'}">home</router-link> |
+  <router-link :to="{name:'article'}">article</router-link>
   <br>
 <router-view></router-view>
 </template>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,15 +1,19 @@
-import {createRouter,createWebHistory} from "vue-router"
+import {createRouter,createWebHistory,createWebHashHistory} from "vue-router"
 import Home from '../views/Home.vue'
 import Article from "../views/Article.vue"
 
 const router = createRouter({
   history:createWebHistory(),
+  // history:createWebHistory('shop'),
+  // history:createWebHashHistory('shop'),
   routes:[{
     path:'/',
+    name:'home',
     component:Home
   },
     {
       path:'/article',
+      name:'article',
       component:Article
     }
   ]
