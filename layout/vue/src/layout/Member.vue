5e0a6fc5cdbb75afeba4b156b51adc25d718e405
Latrevion <xiaopikaqiu@outlook.com>
2023-05-20 20:42:26 +0800

    会员中心跳转

--- layout/vue/src/layout/Member.vue
+++ layout/vue/src/layout/Member.vue
@@ -3,9 +3,38 @@
 </script>
 
 <template>
-  <router-view></router-view>
+  <div class="layout-member">
+    <nav>
+      <router-link :to="{name:'home'}">首页</router-link>
+      <router-link :to="{name:'member'}">修改手机号</router-link>
+      <router-link :to="{name:'email'}">绑定邮箱</router-link>
+    </nav>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.layout-member {
+  display: grid;
+  grid-template-columns: repeat(12,1fr);
 
+  background-color: #f3f3f3;
+  nav{
+    display: flex;
+    flex-direction: column;
+    grid-column: span 3;
+    background: #16a085;
+    padding: 10px;
+    a{
+      color: #ffffff;
+      margin-bottom: 20px;
+    }
+  }
+  main{
+    padding: 20px;
+    grid-column: span 9;
+  }
+}
 </style>
--- layout/vue/src/router/index.js
+++ layout/vue/src/router/index.js
@@ -58,6 +58,7 @@ const router = createRouter({
         },
         {
           path:'email',
+          name:'email',
           component: Email
         }
       ]
