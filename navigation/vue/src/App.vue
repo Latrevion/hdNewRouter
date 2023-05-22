9aa63664d7c9317458f3e6d826ef26881638a936
Latrevion <xiaopikaqiu@outlook.com>
2023-05-22 11:43:36 +0800

    完成页面布局

--- navigation/vue/src/App.vue
+++ navigation/vue/src/App.vue
@@ -3,9 +3,27 @@
 </script>
 
 <template>
+  <router-link :to="{name:'home'}">Home</router-link>
+  <router-link :to="{name:'about'}">About</router-link>
+
+  <div class="view">
+
   <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+a{
+  display: inline-block;
+  color: #fff;
+  background-color: #e67e22;
+  padding: 10px;
+}
+.view{
+  background-color: #f3f3f3;
+  padding: 20px;
+  margin-top: 20px;
+}
+
 
 </style>
