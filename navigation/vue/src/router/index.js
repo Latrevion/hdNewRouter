4fb36eb86ca56a2d2d999481bf7c54e55a36437e
Latrevion <xiaopikaqiu@outlook.com>
2023-05-22 12:03:35 +0800

    beforeEach的Promise

--- navigation/vue/src/router/index.js
+++ navigation/vue/src/router/index.js
@@ -17,4 +17,20 @@ const router = createRouter({
   ]
 })
 
+// router.beforeEach((to, from) => {
+  // console.log(from.name,to.name)
+  // // return false
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve()
+  //   }, 2000)
+  // })
+// })
+
+router.beforeEach(async (to, from) => {
+ // return  await new Promise(resolve => resolve(false))
+ return  await new Promise(resolve => resolve(true))
+  // return false
+  // return true
+})
 export default router
