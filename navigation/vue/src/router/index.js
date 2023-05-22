3fdccdf6480464928f3656ecacbf85b81da00a74
Latrevion <xiaopikaqiu@outlook.com>
2023-05-22 11:29:14 +0800

    页面布局

--- navigation/vue/src/router/index.js
+++ navigation/vue/src/router/index.js
@@ -1,8 +1,20 @@
-import {createRouter,createWebHistory} from "vue-router"
+import {createRouter, createWebHistory} from "vue-router"
+import Home from "@/views/Home.vue"
+import About from "@/views/About.vue"
 
 const router = createRouter({
-  history:createWebHistory(),
-  routes:[{}]
+  history: createWebHistory(),
+  routes: [{
+    path: "/home",
+    name: "home",
+    component: Home
+  },
+    {
+      path: "/about",
+      name: "about",
+      component: About
+    }
+  ]
 })
 
 export default router
