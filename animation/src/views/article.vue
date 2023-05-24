b9d0bd058c7048d9423f2973b8ccc46d12cc301e
Latrevion <xiaopikaqiu@outlook.com>
2023-05-24 17:44:33 +0800

    通过watch获取文章内容

--- animation/src/views/article.vue
+++ animation/src/views/article.vue
@@ -1,14 +1,26 @@
 <script setup>
-import {ref} from "vue"
+import {ref, watch} from "vue"
+import {useRoute} from "vue-router"
 
-const lists = ref()
+const route = useRoute()
+const lists = ref({})
+const article = ref({})
 
 lists.value = await fetch("http://127.0.0.1:3003/article").then(r => r.json())
 
+watch(() => route.query, async (newQuery) => {
+  article.value= await fetch(`http://127.0.0.1:3003/article/${newQuery.id ?? 1}`).then(r => r.json())
+}, {immediate: true})
 </script>
 
 <template>
   <div class="page">
+    <div>
+      <h2>{{article.title}}</h2>
+      <p>
+      {{ article.content }}
+      </p>
+    </div>
     <div class="lists">
       <router-link :to="{name:'article',query:{id:article.id}}" v-for="article in lists" :key="article.id">
         {{ article.title }}
@@ -17,10 +29,15 @@ lists.value = await fetch("http://127.0.0.1:3003/article").then(r => r.json())
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .page {
+  display: flex;
+
   .lists {
+    min-width: 300px;
+    margin-left: 20px;
     a {
+      font-size: .8rem;
       display: block;
       background-color: #17a085;
       color: #ffffff;
