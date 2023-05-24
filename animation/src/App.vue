6663dc910be924195e15a13064fa9d94ccc9fbe7
Latrevion <xiaopikaqiu@outlook.com>
2023-05-24 11:41:22 +0800

    不同模式的动画效果

--- animation/src/App.vue
+++ animation/src/App.vue
@@ -8,23 +8,57 @@
     <router-link :to="{name:'about'}">about</router-link>
   </div>
   <div class="router-view">
-  <router-view></router-view>
+    <router-view #default="{Component}">
+      <transition name="fade">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <style lang="scss">
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+
+.fade-enter-active {
+  transition: all 1s ease;
+}
+
+.fade-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.fade-leave-from {
+  transform: translateX(0);
+
+}
+
+.fade-leave-active {
+  transition: all 1s ease;
+}
+
+.fade-leave-to {
+  transform: translateX(-100%);
+
+}
+
+
 .link {
   a {
     display: inline-block;
     background-color: #16a085;
     color: #fff;
-    margin-right:  10px;
+    margin-right: 10px;
     text-align: center;
     padding: 10px;
     text-decoration: none;
   }
 }
-.router-view{
+
+.router-view {
   margin-top: 20px;
 }
 </style>
--- animation/src/views/about.vue
+++ animation/src/views/about.vue
@@ -3,8 +3,23 @@
 </script>
 
 <template>
-<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, hic.</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ex explicabo fuga incidunt maxime odio omnis totam! Alias aliquid assumenda cum impedit neque, perferendis quod velit. Alias aut autem dicta dolore excepturi fuga illum ipsa iure laborum maxime minima molestiae neque nisi officiis omnis perspiciatis, provident quod recusandae reiciendis rem sint sit suscipit tempore tenetur ullam veritatis, voluptatem voluptatum. Alias atque consectetur distinctio ducimus ea earum inventore mollitia nihil non nulla perspiciatis, provident, quibusdam quos sapiente unde, veritatis voluptas. Ipsa itaque officiis ratione recusandae suscipit? Consequatur corporis delectus doloremque eaque earum eum fugiat illum iste iure laudantium officia perferendis porro quaerat quo ratione recusandae rem sapiente tempore velit, voluptatum? Aliquam assumenda illo, molestias placeat sed unde veniam! Amet autem culpa cum, cupiditate dolore ea, eligendi eveniet excepturi explicabo id in mollitia numquam odio praesentium rem soluta, tenetur veniam voluptas voluptate voluptatem! Aliquid aperiam assumenda consequatur delectus ea iste maxime natus numquam odio officia placeat porro quae quas ratione, recusandae repellendus rerum suscipit tenetur, velit vero vitae voluptate voluptatum! Ab aliquam amet asperiores autem commodi consequuntur debitis dolor dolore, error, est facilis fugiat id incidunt itaque laboriosam maiores molestiae non nulla odio quam quos repellendus rerum sed sint unde voluptatem voluptatum.</p>
+  <div class="page">
+    <h2>About --- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, hic.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ex explicabo fuga incidunt maxime odio omnis
+      totam! Alias aliquid assumenda cum impedit neque, perferendis quod velit. Alias aut autem dicta dolore excepturi
+      fuga illum ipsa iure laborum maxime minima molestiae neque nisi officiis omnis perspiciatis, provident quod
+      recusandae reiciendis rem sint sit suscipit tempore tenetur ullam veritatis, voluptatem voluptatum. Alias atque
+      consectetur distinctio ducimus ea earum inventore mollitia nihil non nulla perspiciatis, provident, quibusdam quos
+      sapiente unde, veritatis voluptas. Ipsa itaque officiis ratione recusandae suscipit? Consequatur corporis delectus
+      doloremque eaque earum eum fugiat illum iste iure laudantium officia perferendis porro quaerat quo ratione
+      recusandae rem sapiente tempore velit, voluptatum? Aliquam assumenda illo, molestias placeat sed unde veniam! Amet
+      autem culpa cum, cupiditate dolore ea, eligendi eveniet excepturi explicabo id in mollitia numquam odio
+      praesentium rem soluta, tenetur veniam voluptas voluptate voluptatem! Aliquid aperiam assumenda consequatur
+      delectus ea iste maxime natus numquam odio officia placeat porro quae quas ratione, recusandae repellendus rerum
+      suscipit tenetur, velit vero vitae voluptate voluptatum! Ab aliquam amet asperiores autem commodi consequuntur
+      debitis dolor dolore, error, est facilis fugiat id incidunt itaque laboriosam maiores molestiae non nulla odio
+      quam quos repellendus rerum sed sint unde voluptatem voluptatum.</p>
+  </div>
 </template>
 
 <style scoped>
