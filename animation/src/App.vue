d17bc3abc13d78a548d99280971f3dd059f0c221
Latrevion <xiaopikaqiu@outlook.com>
2023-05-24 11:08:02 +0800

    路由跳转与样式声明

--- animation/src/App.vue
+++ animation/src/App.vue
@@ -3,9 +3,28 @@
 </script>
 
 <template>
+  <div class="link">
+    <router-link :to="{name:'home'}">home</router-link>
+    <router-link :to="{name:'about'}">about</router-link>
+  </div>
+  <div class="router-view">
   <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+.link {
+  a {
+    display: inline-block;
+    background-color: #16a085;
+    color: #fff;
+    margin-right:  10px;
+    text-align: center;
+    padding: 10px;
+    text-decoration: none;
+  }
+}
+.router-view{
+  margin-top: 20px;
+}
 </style>
--- animation/src/router/index.js
+++ animation/src/router/index.js
@@ -1,8 +1,21 @@
-import {createRouter,createWebHistory} from "vue-router"
+import {createRouter, createWebHistory} from "vue-router"
+import home from "@/views/home.vue"
+import about from "@/views/about.vue"
 
 const router = createRouter({
-  history:createWebHistory(),
-  routes:[]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: home
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: about
+    }
+  ]
 })
 
-export default  router
+export default router
